f526c28b8d48e2f658d86afac1d1b2a68ac635c6
josepjunior <josepsjunior.df@gmail.com>
2021-02-18 01:47:21 -0300

    Implementação de sessão

--- src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.acao.Acao;
 
@@ -26,21 +27,29 @@ public class UnicaEntradaServlet extends HttpServlet {
 			throws ServletException, IOException {
 
 		String paramAcao = request.getParameter("acao");
+
+		HttpSession sessao = request.getSession();
+		boolean usuarioNaoEstaLogado = (sessao.getAttribute("usuarioLogado") == null);
+		boolean ehUmaAcaoProtegida = !(paramAcao.equals("Login") || paramAcao.equals("LoginForm"));
+		if (ehUmaAcaoProtegida & usuarioNaoEstaLogado) {
+			response.sendRedirect("entrada?acao=LoginForm");
+			return;
+		}
+
 		String nomeDaClasse = "br.com.alura.gerenciador.acao." + paramAcao;
 
 		String nome;
 		try {
-		    Class classe = Class.forName(nomeDaClasse);//carrega a classe com o nome 
-		    Acao acao = (Acao) classe.newInstance(); 
-		    nome = acao.executa(request, response);
+			Class classe = Class.forName(nomeDaClasse);// carrega a classe com o nome
+			Acao acao = (Acao) classe.newInstance();
+			nome = acao.executa(request, response);
 		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-		    throw new ServletException(e);
+			throw new ServletException(e);
 		}
-	    
 
 		String[] tipoEEndereco = nome.split(":");
 		if (tipoEEndereco[0].equals("forward")) {
-			RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/view/"+tipoEEndereco[1]);
+			RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/view/" + tipoEEndereco[1]);
 			rd.forward(request, response);
 		} else {
 			response.sendRedirect(tipoEEndereco[1]);
